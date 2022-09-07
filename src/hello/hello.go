01918762480a9adcb2f81407337523921bebc60f
Juliano_Ric <julianorics@hotmail.com>
2022-09-07 20:31:01 -0300

    Aprendendo if, else, switch e funcoes em Go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,26 +6,68 @@
 package main
 
 import "fmt"
+import  "os"
 
 func main(){
-   //nao eh preciso tipar a variavel, o Go infere sozinho	
-   var versao float32 = 1.1
-   //outra forma de declarar variavel é  :=   
-   nome := "Juliano"
-   
-   fmt.Println("Hello Mister", nome)
-   fmt.Println("Este programa está  na versão: ", versao)
-   
-   fmt.Println("1-Iniciar monitorament")
-   fmt.Println("2-Exibir Logs")
-   fmt.Println("0-Sair do programa")
+    
+   exibeIntroducao()
+   exibeMenu()
 
    //lendo a opcao do usuario
-   var comando int
-   //fmt.Scanf("%d", &comando)
+   comando := leComando()
+    
+  //controle de fluxo com if-else
+  /************************************ 
+   if comando == 1{
+	  fmt.Println("Monitorando...")
+   }else if comando == 2{
+      fmt.Println("Exibindo logs...")
+   }else if comando == 0{
+       fmt.Println("Saindo do programa")
+   }else{
+    fmt.Println("Yo no lo conozco senor")
+   }
+}
+********************************************/
 
-   //de uma forma melhor (sem o modificador)
-   fmt.Scan(&comando)
+//controle de fluxo com switch
+switch comando{
+    case 1: 
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa..")
+		os.Exit(0)
+	default:
+		fmt.Println("Yo no lo conozco senor")	
+		os.Exit(-1)
+}
 
-   fmt.Println("O comando escolhido foi", comando)
+}
+
+//trabalhando com funcoes
+
+func exibeIntroducao(){
+    //nao eh preciso tipar a variavel, o Go infere sozinho	
+    //outra forma de declarar variavel é  :=   
+	var versao float32 = 1.1
+	nome := "Juliano"
+	fmt.Println("Hello Mister", nome)
+	fmt.Println("Este programa está  na versão: ", versao)
+}
+
+func leComando() int{
+	var comandoLido int
+	//fmt.Scanf("%d", &comandoLido)
+    //de uma forma melhor (sem o modificador)
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+	return comandoLido
+}
+
+func exibeMenu(){
+   fmt.Println("1-Iniciar monitoramento")
+   fmt.Println("2-Exibir Logs")
+   fmt.Println("0-Sair do programa")
 }
