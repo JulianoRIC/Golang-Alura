aa42b095d0f8177d72e259ed495d02db930ff301
Juliano_Ric <julianorics@hotmail.com>
2022-09-07 19:50:51 -0300

    hello world em andamento

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,5 +8,24 @@ package main
 import "fmt"
 
 func main(){
-   fmt.Println("Hello world!")
+   //nao eh preciso tipar a variavel, o Go infere sozinho	
+   var versao float32 = 1.1
+   //outra forma de declarar variavel é  :=   
+   nome := "Juliano"
+   
+   fmt.Println("Hello Mister", nome)
+   fmt.Println("Este programa está  na versão: ", versao)
+   
+   fmt.Println("1-Iniciar monitorament")
+   fmt.Println("2-Exibir Logs")
+   fmt.Println("0-Sair do programa")
+
+   //lendo a opcao do usuario
+   var comando int
+   //fmt.Scanf("%d", &comando)
+
+   //de uma forma melhor (sem o modificador)
+   fmt.Scan(&comando)
+
+   fmt.Println("O comando escolhido foi", comando)
 }
